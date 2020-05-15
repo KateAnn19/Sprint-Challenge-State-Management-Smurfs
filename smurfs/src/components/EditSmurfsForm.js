fd5de075385f87a6a46dd11f653fc1bf34d73b24
Kate McGee <mcgeehee@gmail.com>
2020-05-15 13:40:16 -0600

    update working better

--- smurfs/src/components/EditSmurfsForm.js
+++ smurfs/src/components/EditSmurfsForm.js
@@ -1,88 +1,80 @@
-import React, { useState} from "react";
-import { connect, useDispatch } from "react-redux";
+import React, { useState } from "react";
+import { connect} from "react-redux";
 
 import { updateSmurfs } from "../store/actions";
 
-
-function EditSmurfsForm({age, name, height, id, toggleEditForm}) {
-    const dispatch = useDispatch();
- const [formState, setFormState] = useState({
+function EditSmurfsForm({ age, name, height, id, toggleEditForm, updateSmurfs }) {
+  
+  const [formState, setFormState] = useState({
     name: name,
     age: age,
-    height: height
+    height: height,
   });
 
-
-  const formSubmit = e => {
+  const formSubmit = (e) => {
     e.preventDefault();
-    dispatch(updateSmurfs({...formState, id}));
+    updateSmurfs({ ...formState, id });
     toggleEditForm(false);
     setFormState({
-        name: "",
-        age: "",
-        height: ""
-      });
-   
+      name: "",
+      age: "",
+      height: "",
+    });
   };
 
-  const inputChange = e => {
-    e.persist();
+  const inputChange = (e) => {
+    //e.persist();
     const newFormData = {
       ...formState,
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
     };
-    
+
     setFormState(newFormData);
   };
 
-
   return (
     <form onSubmit={formSubmit}>
-    <label htmlFor="name">
-      Name
-      <input
-        id="name"
-        type="text"
-        name="name"
-        value={formState.name}
-        onChange={inputChange}
-      />
-     
-    </label>
-    <label htmlFor="age">
-      Age
-      <input
-        id="age"
-        type="number"
-        name="age"
-        value={formState.age}
-        onChange={inputChange}
-      />
-      
-    </label>
-    <label htmlFor="height">
-      Height
-      <input
-        id="height"
-        type="text"
-        name="height"
-        value={formState.height}
-        onChange={inputChange}
-      />
-     
-    </label>    
-    <button>Submit Updated Smurf</button>
-  </form>
+      <label htmlFor="name">
+        Name
+        <input
+          id="name"
+          type="text"
+          name="name"
+          value={formState.name}
+          onChange={inputChange}
+        />
+      </label>
+      <label htmlFor="age">
+        Age
+        <input
+          id="age"
+          type="number"
+          name="age"
+          value={formState.age}
+          onChange={inputChange}
+        />
+      </label>
+      <label htmlFor="height">
+        Height
+        <input
+          id="height"
+          type="text"
+          name="height"
+          value={formState.height}
+          onChange={inputChange}
+        />
+      </label>
+      <button>Submit Updated Smurf</button>
+    </form>
   );
 }
 
 const mapStateToProps = (state) => {
-   
-    return {
-      isFetching: state.isFetching,
-      smurfs: state.smurfs,
-      error: state.error,
-    };
+  return {
+    isFetching: state.isFetching,
+    smurfs: state.smurfs,
+    error: state.error,
   };
-  
-  export default connect(mapStateToProps, { updateSmurfs })(EditSmurfsForm);
+};
+
+export default connect(mapStateToProps, { updateSmurfs })(EditSmurfsForm);
